79089cd6d5169e91ac99647a610220416a99c970
192110686 <kanderkrs2003@gmail.com>
2026-03-07 20:34:16 +0530

    Java Programs

---
+++ PrintDemo.java
@@ -0,0 +1,11 @@
+public class PrintDemo {
+    /* print/println/printf */
+    /*print vs println */
+    public static void main(String[] args){
+        System.out.print("A");
+        System.out.print("A");
+        System.out.println();
+        System.out.println("C");
+        System.out.println("D");
+    }
+}
